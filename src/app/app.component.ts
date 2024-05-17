df85ce48c1af08b6be7274a86e8862c1802b03e6
taqiniazi <taqiniazi61@gmail.com>
2024-05-18 02:03:01 +0500

    Updated Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './common/sidebar/sidebar.component';
 import { HeaderComponent } from './common/header/header.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
-
+import { Component, Inject, HostListener,PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -21,7 +21,25 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   title = 'phc-new';
-  sidebarView = false;
+  sidebarView = true;
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.sidebarView = window.innerWidth > 768;
+      this.onResize();
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: Event) {
+    if (isPlatformBrowser(this.platformId)) {
+      if (window.innerWidth <= 768) {
+        this.sidebarView = false;
+      } else {
+        this.sidebarView = true;
+      }
+    }
+  }
+  
   toggleSidebarView() {
     this.sidebarView = !this.sidebarView;
   }
